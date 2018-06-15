cd2328bbd91aa6a460e3f2897ba0a7a8cff0ae5e
Marios Papachristou <papachristoumarios@gmail.com>
2018-06-15 13:50:00 +0300

    converter.py supports argparse
    Former-commit-id: 780af59fbc3c5fdc31870148f4fe27bdde289f70
    Former-commit-id: 335ac7a6b3e8fae213a263665fc9f136be91b035

--- scripts/converter.py
+++ scripts/converter.py
@@ -5,24 +5,45 @@
 
 import os
 import multiprocessing
+import argparse
+
+def job(x):
+		global pdf2txt
+		global output_dir
+		y = x.replace('.pdf', '.txt')
+		y = output_dir  + y.split('/')[-1]
+		if not os.path.isfile(y):
+				os.system('{} {} > {}'.format(pdf2txt, x, y))
+				print('Done')
+		else:
+				print('already a file')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-pdf2txt', help='pdf2txt.py Executable')
+parser.add_argument('-input_dir', help='Input Directory')
+parser.add_argument('-output_dir', help='Output Directory')
+
+args = parser.parse_args()
+
+global input_dir
+global output_dir
+global pdf2txt
+input_dir = args.input_dir
+output_dir = args.output_dir
+pdf2txt = args.pdf2txt
+
+if not output_dir.endswith('/'):
+	output_dir = output_dir + '/'
 
 pdfs = []
 
-for root, dirs, files in os.walk('/home/repos/GGG'):
-        for file in files:
-                if file.endswith('.pdf'):
-                        pdfs.append(os.path.join(root, file))
+for root, dirs, files in os.walk(input_dir):
+		for file in files:
+				if file.endswith('.pdf'):
+						pdfs.append(os.path.join(root, file))
 
 
 
-def job(x):
-        y = x.replace('.pdf', '.txt')
-        y = './output/' + y.split('/')[-1]
-        if not os.path.isfile(y):
-                os.system('python3 /home/marios/pdfminer.six/tools/pdf2txt.py {} > {}'.format(x, y))
-        else:
-                print('already a file')
-        print('Done')
 
 pool = multiprocessing.Pool(5)
 pool.map(job, pdfs)
