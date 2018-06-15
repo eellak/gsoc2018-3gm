d2cdca76e79231b957eaffe912c64aea0f07f552
Marios Papachristou <papachristoumarios@gmail.com>
2018-06-15 17:10:43 +0300

    Formated scripts under pep8 standard
    Former-commit-id: 387b290f8463301641eadf49d9a66a9015d5dad7
    Former-commit-id: e76d5ae66fa732f8197cfd8e5a83c72f9b378ccc

--- scripts/converter.py
+++ scripts/converter.py
@@ -10,23 +10,25 @@ import argparse
 
 MIN_BYTES = 200
 
-def job(x):
-		global pdf2txt
-		global output_dir
-		y = x.replace('.pdf', '.txt')
-		y = output_dir  + y.split('/')[-1]
-		if not os.path.isfile(y):
-				os.system('{} {} > {}'.format(pdf2txt, x, y))
-				if os.stat(y).st_size <= MIN_BYTES:
-					print('File Size unsatisfactory. Performing OCR')
-					try:
-						ocr.pdfocr2txt(x, y)
-					except:
-						print('OCR Failed')	
 
-				print('Done')
-		else:
-				print('already a file')
+def job(x):
+    global pdf2txt
+    global output_dir
+    y = x.replace('.pdf', '.txt')
+    y = output_dir + y.split('/')[-1]
+    if not os.path.isfile(y):
+        os.system('{} {} > {}'.format(pdf2txt, x, y))
+        if os.stat(y).st_size <= MIN_BYTES:
+            print('File Size unsatisfactory. Performing OCR')
+            try:
+                ocr.pdfocr2txt(x, y)
+            except BaseException:
+                print('OCR Failed')
+
+        print('Done')
+    else:
+        print('already a file')
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-pdf2txt', help='pdf2txt.py Executable')
@@ -43,16 +45,14 @@ output_dir = args.output_dir
 pdf2txt = args.pdf2txt
 
 if not output_dir.endswith('/'):
-	output_dir = output_dir + '/'
+    output_dir = output_dir + '/'
 
 pdfs = []
 
 for root, dirs, files in os.walk(input_dir):
-		for file in files:
-				if file.endswith('.pdf'):
-						pdfs.append(os.path.join(root, file))
-
-
+    for file in files:
+        if file.endswith('.pdf'):
+            pdfs.append(os.path.join(root, file))
 
 
 pool = multiprocessing.Pool(5)
