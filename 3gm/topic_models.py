5e7db5139f0f0189458925767f31c91ed4ae8052
Marios Papachristou <papachristoumarios@gmail.com>
2018-07-13 12:50:02 +0300

    Tidy code in topic_models.py
    Former-commit-id: 56f038e9260af37e4873ff043392c8eba77cd2af
    Former-commit-id: 5416bbef317debf1ad922bc075bb4c1c769ecbd1

--- 3gm/topic_models.py
+++ 3gm/topic_models.py
@@ -1,5 +1,4 @@
-from __future__ import print_function
-from time import time
+from multiprocessing import cpu_count
 
 # Matplotlib and scikit
 import matplotlib.pyplot as plt
@@ -17,6 +16,7 @@ import pprint
 import re
 import codifier
 import database
+import math
 
 
 sys.path.insert(0, '../resources')
@@ -41,6 +41,8 @@ def process_topics(
 		no_top_data_samples):
 	graph = {}
 	topics = {}
+	global db
+	db.drop_topics()
 	for topic_idx, topic in enumerate(H):
 
 
@@ -69,7 +71,6 @@ def process_topics(
 
 		}
 
-		global db
 
 		db.topics.save(s)
 
@@ -79,180 +80,115 @@ def process_topics(
 	print(topics)
 	return graph, topics, top_doc_indices
 
+def build_greek_stoplist(cnt_swords = 300):
+	greek_stopwords = []
+
+	with open('../resources/greek_stoplist.dat') as f:
+		for i in range(cnt_swords):
+			line = f.readline()
+			if not line:
+				break
+			line = line.split(' ')
+			greek_stopwords.append(line[0])
+
+	return greek_stopwords
+
+
+def build_data_samples(min_size=4):
+	data_samples = []
+	indices = {}
+
+	i = 0
+	for law in codifier.codifier.laws.keys():
+		print(law)
+		corpus = codifier.codifier.get_law(law, export_type='str')
+		tmp = corpus.split(' ')
+		corpus = []
+		for j, word in enumerate(tmp):
+			if contains_digit(tmp[j]) or len(tmp[j]) < min_size:
+				continue
+			try:
+				# FUTURE: Use spaCy's lemmatizer
+				corpus.append(greek_lemmas.lemmas[word])
+			except BaseException:
+				corpus.append(word)
+
+		corpus = ' '.join(corpus)
+
+		data_samples.append(corpus)
+		indices[i] = law
+
+		i += 1
+
+	return data_samples, indices
+
+def build_gg_stoplist(data_samples, greek_stopwords, gg_most_common = 500):
+	words = []
+	for x in data_samples:
+		words.extend(x.split(' '))
+	print('Counting words')
+	counter = collections.Counter(words)
+	for w in counter.most_common(gg_most_common):
+		greek_stopwords.append(w[0])
+	print('Done Counting')
+	return greek_stopwords, words
 
 
 
-greek_stopwords = []
-cnt_swords = 300
+def display_components(graph_lda):
+	print('\nBreadth first Search for Connected Components for Latent Dirichlet Allocation')
+	cc_lda = connected_components(graph_lda)
+	print(cc_lda)
+	print('Statutes')
+	for c in cc_lda:
+		print([codifier.codifier.laws[indices[d]] for d in c])
 
-with open('../resources/greek_stoplist.dat') as f:
-	for i in range(cnt_swords):
-		line = f.readline()
-		if not line:
-			break
-		line = line.split(' ')
-		greek_stopwords.append(line[0])
+def main():
+	greek_stopwords = build_greek_stoplist()
+	data_samples, indices = build_data_samples()
+	greek_stopwords, words = build_gg_stoplist(data_samples, greek_stopwords)
 
+	# Initial Parameters
+	no_features = 1000 # Number of features
+	n_samples = len(data_samples) # Len of data samples
+	no_top_words = 100 # Number of top words in each topic
+	n_components = 100 # Number of topics
+	no_top_data_samples = math.ceil(n_samples / n_components) # How many correlations under each topic
 
-
-data_samples = []
-indices = {}
-
-min_size = 4
-
-i = 0
-for law in codifier.codifier.laws.keys():
-	print(law)
-	corpus = codifier.codifier.get_law(law)
-	tmp = corpus.split(' ')
-	corpus = []
-	for j, word in enumerate(tmp):
-		if contains_digit(tmp[j]) or len(tmp[j]) < min_size:
-			continue
-		try:
-			corpus.append(greek_lemmas.lemmas[word])
-		except BaseException:
-			corpus.append(word)
-
-	corpus = ' '.join(corpus)
-
-	data_samples.append(corpus)
-	indices[i] = law
-
-	i += 1
-
-words = []
-
-for x in data_samples:
-	words.extend(x.split(' '))
-
-print('Counting words')
-counter = collections.Counter(words)
-gg_most_common = 350
-for w in counter.most_common(gg_most_common):
-	greek_stopwords.append(w[0])
-
-print('Done')
+	# LDA can only use raw term counts for LDA because it is a probabilistic
+	# graphical model
+	tf_vectorizer = CountVectorizer(
+		max_df=0.95,
+		min_df=2,
+		max_features=no_features,
+		stop_words=greek_stopwords)
+	tf = tf_vectorizer.fit_transform(data_samples)
+	tf_feature_names = tf_vectorizer.get_feature_names()
 
 
-# Initial Parameters
-no_features = 1000
-no_top_words = 5
-n_samples = len(data_samples)
+	lda_model = LatentDirichletAllocation(n_components=n_components,
+									max_iter=10,
+	                                learning_method='online',
+	                                learning_offset=50.,
+									verbose=1,
+									n_jobs= cpu_count() - 1,
+	                                random_state=0)
+	lda_model.fit(tf)
 
-# NMF is able to use tf-idf
-tfidf_vectorizer = TfidfVectorizer(
-	max_df=0.95,
-	min_df=2,
-	max_features=no_features,
-	stop_words=greek_stopwords)
-tfidf = tfidf_vectorizer.fit_transform(data_samples)
-tfidf_feature_names = tfidf_vectorizer.get_feature_names()
-
-# LDA can only use raw term counts for LDA because it is a probabilistic
-# graphical model
-tf_vectorizer = CountVectorizer(
-	max_df=0.95,
-	min_df=2,
-	max_features=no_features,
-	stop_words=greek_stopwords)
-tf = tf_vectorizer.fit_transform(data_samples)
-tf_feature_names = tf_vectorizer.get_feature_names()
+	print("Best Perplexity Score: ", lda_model.perplexity(tf))
 
 
-# Run NMF
-nmf_model = NMF(
-	n_components=1000,
-	random_state=1,
-	alpha=.1,
-	l1_ratio=.5,
-	init='nndsvd').fit(tfidf)
-nmf_W = nmf_model.transform(tfidf)
-nmf_H = nmf_model.components_
-
-# Run LDA after finding optimal parameters
-model = GridSearchCV(
-	cv=None,
-	error_score='raise',
-	estimator=LatentDirichletAllocation(
-		batch_size=128,
-		doc_topic_prior=None,
-		evaluate_every=-1,
-		learning_decay=0.7,
-		learning_method='batch',
-		learning_offset=10.0,
-		max_doc_update_iter=100,
-		max_iter=10,
-		mean_change_tol=0.001,
-		n_components=None,
-		n_jobs=1,
-		perp_tol=0.1,
-		random_state=None,
-		topic_word_prior=None,
-		total_samples=1000000.0,
-		verbose=0),
-	fit_params=None,
-	iid=True,
-	n_jobs=1,
-	param_grid={ # change e.g. to range() for grid search
-		'n_components': [10],
-		'learning_decay': [
-			0.5,
-			0.7,
-			0.9],
-		'n_samples' : [2000],
-		'n_features'  : [1000]
-
-		},
-	pre_dispatch='2*n_jobs',
-	refit=True,
-	return_train_score='warn',
-	scoring=None,
-	verbose=0)
-model.fit(tfidf)
-
-# Best Model
-lda_model = model.best_estimator_
-
-# Model Parameters
-print('LDA Model')
-print("Best Model's Params: ", model.best_params_)
-
-# Log Likelihood Score from grid search
-print("Best Log Likelihood Score: ", model.best_score_)
-print("Best Perplexity Score: ", lda_model.perplexity(tfidf))
+	lda_W = lda_model.transform(tf)
+	lda_H = lda_model.components_
 
 
-lda_W = lda_model.transform(tf)
-lda_H = lda_model.components_
+	graph_lda, topics, top_doc_indices = process_topics(
+		lda_H,
+		lda_W,
+		tf_feature_names,
+		data_samples,
+		no_top_words,
+		no_top_data_samples)
 
-no_top_words = 10
-no_top_data_samples = 3
-graph_nmf, topics, top_doc_indices = process_topics(
-	nmf_H,
-	nmf_W,
-	tfidf_feature_names,
-	data_samples,
-	no_top_words,
-	no_top_data_samples)
-graph_lda, topics, top_doc_indices = process_topics(
-	lda_H,
-	lda_W,
-	tf_feature_names,
-	data_samples,
-	no_top_words,
-	no_top_data_samples)
-
-print('Breadth first Search for Connected Components for NMF')
-cc_nmf = connected_components(graph_nmf)
-print(cc_nmf)
-print('Filenames')
-for c in cc_nmf:
-	print([codifier.codifier.laws[indices[d]] for d in c])
-
-print('\nBreadth first Search for Connected Components for Latent Dirichlet Allocation')
-cc_lda = connected_components(graph_lda)
-print(cc_lda)
-print('Filenames')
-for c in cc_lda:
-	print([codifier.codifier.laws[indices[d]] for d in c])
+if __name__ == '__main__':
+	main()
