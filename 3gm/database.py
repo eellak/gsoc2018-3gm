efb60962e7f87f26d9eab1c87aa45c8468de2fb4
Marios Papachristou <papachristoumarios@gmail.com>
2018-07-17 14:22:39 +0300

    Add rollback methods for links and documentation
    - Rollback methods for links
    - Document database wrapper
    
    
    Former-commit-id: abb58be1133cde076e4d8877149dd3e4b924912d
    Former-commit-id: 0136ebadf77f4bd8c8419b18f1ac362d1d1ef4fe

--- 3gm/database.py
+++ 3gm/database.py
@@ -21,8 +21,10 @@ except pymongo.errors.ConnectionFailure as e:
 
 
 class Database:
+    """Database Wrapper Class. Serves for database wrapping"""
 
     def __init__(self):
+        """Database wrapper constructor"""
         self.db = client['3gmdb']
         self.issues = self.db.issues
         self.laws = self.db.laws
@@ -30,6 +32,7 @@ class Database:
         self.topics = self.db.topics
 
     def insert_issue_to_db(self, issue):
+        """Inserts issue to database"""
         issue.detect_signatories()
         serializable = {
             'issue_date': str(
@@ -49,24 +52,26 @@ class Database:
         self.issues.insert(serializable)
 
     def print_laws(self):
+        """Print laws in the database with pprint"""
         cursor = self.laws.find({})
         for x in cursor:
             pprint.pprint(x)
 
     def drop_laws(self):
+        """Drop laws collection"""
         self.db.drop_collection('laws')
 
     def drop_issues(self):
+        """Drop issues collection"""
         self.db.drop_collection('issues')
 
     def push_law_to_db(self, law):
+        """Push law to database via serializing it
+        :params law LawParser object"""
         self.laws.save(law.serialize())
 
-    def parse_and_push_law(self, identifier, fileneme):
-        law = parser.LawParser(filename, identifier)
-        self.push_law_to_db(law.__dict__())
-
     def query_from_tree(self, law, tree, issue_name=None):
+        """Apply query from tree"""
         print('Querying from tree')
         result = law.query_from_tree(tree)
         result['_version'] = law.version_index
@@ -92,16 +97,21 @@ class Database:
         self.laws.save(temp)
 
     def insert_links(self, links):
+        """Insert links to database"""
         for link in links:
             self.links.save(link.serialize())
 
     def drop_links(self):
+        """Drop links collection"""
         self.db.drop_collection('links')
 
     def drop_topics(self):
+        """Drop topics collection"""
         self.db.drop_collection('topics')
 
     def rollback_laws(self, identifier=None):
+        """Rollback laws
+        :param identifier If None rollback everything else rollback certain id"""
         if identifier:
             cursor = self.laws.find({'_id' : identifier})
         else:
@@ -118,3 +128,32 @@ class Database:
             # Version 0 does not exist
             except IndexError:
                 pass
+
+    def rollback_links(self, identifier=None, rollback_laws=False):
+        """Rollback links
+        :param identifier If none rollback everything else rollback certain id
+        :param rollback_laws if true rollback laws
+        """
+        if identifier:
+            cursor = self.links.find({
+                '_id' : identifier,
+                'actual_links.status' : 'εφαρμοσμένος'
+            })
+        else:
+            cursor = self.links.find({
+                'actual_links.status' : 'εφαρμοσμένος'
+            })
+
+        for x in cursor:
+            tmp = copy.copy(x)
+            for y in tmp['actual_links']:
+                y['status'] = 'μη εφαρμοσμένος'
+
+            self.links.save(tmp)
+
+        if rollback_laws:
+            self.rollback_laws(identifier=identifier)
+
+    def rollback_all():
+        """Rollsback everything in the database"""
+        self.rollback_links(identifier=None, rollback_laws=True)
