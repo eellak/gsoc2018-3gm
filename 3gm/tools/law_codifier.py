1d7ab7b9566aa11c890dd724cfaae185915a041d
Marios Papachristou <papachristoumarios@gmail.com>
2018-07-18 01:56:25 +0300

    Tools directory for tools
    - Moved law_codifier.py to tools
    
    
    Former-commit-id: 996ae36a9f032648db688acbc30b740e689daab8
    Former-commit-id: 18e311608e2cc62befb66e35aff5621d7e7e2df1

---
+++ 3gm/tools/law_codifier.py
@@ -0,0 +1,26 @@
+import sys
+sys.path.insert(0, '../') 
+import codifier
+import argparse
+
+if __name__ == '__main__':
+    argparser = argparse.ArgumentParser(
+        description='''This is the command line tool for codifying documents''')
+
+    optional = argparser.add_argument_group('Optional arguments')
+
+    optional.add_argument(
+        '--source',
+        help='Source Statute')
+    optional.add_argument(
+        '--target',
+        help='Target Statute'
+    )
+
+    optional.add_argument(
+        '--output',
+        help='Output file'
+    )
+
+    args = argparser.parse_args()
+    codifier.LawCodifier.codify_pair(args.source, args.target, args.output)
