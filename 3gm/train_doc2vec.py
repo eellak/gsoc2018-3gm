258832b43e1719180f94c96f30df39b84908cf24
Marios Papachristou <papachristoumarios@gmail.com>
2018-07-18 00:13:46 +0300

    Add tags support
    Former-commit-id: fb95126ff1be9374536b9364ea8c6826433a1a49
    Former-commit-id: 375b080375e74e286a2050597f06392622189852

--- 3gm/train_doc2vec.py
+++ 3gm/train_doc2vec.py
@@ -1,36 +1,32 @@
 #!/usr/bin/env python3
 # Train word2vec model and (optioanallly) create document embeddings for the dataset
-# usage: train_doc2vec.py corpus.txt model.bin --embeddings --tokenize > embeddings.txt
-# or (with labeling): train_doc2vec.py corpus.txt model.bin --embeddings --tokenize | label_embeddings.py labels.txt output.pickle
+# usage: train_doc2vec.py corpus.txt model.bin labels.txt --tokenize
 
 import gensim.models as g
 import logging
-import multiprocessing
 import sys
 import tokenizer
 from gensim.models.doc2vec import TaggedDocument
-from infer_doc2vec import infer
+from multiprocessing import cpu_count
 
 # document cleanup
 def nlp_clean(data):
 	new_data = []
 	for d in data:
-		new_str = d.lower()
-		dlist = tokenizer.tokenizer.split(d, delimiter='. ')
-		new_data.append(dlist)
-	return new_data
+		new_str = d.lower().strip()
+		dlist = tokenizer.tokenizer.split(d, delimiter=' ')
+		yield dlist
 
 #doc2vec parameters
 vector_size = 150
-window_size = 15
-min_count = 1
+window_size = 8
+min_count = 4
 sampling_threshold = 1e-5
 negative_size = 5
-train_epoch = 100
+train_epoch = 50
 dm = 0 #0 = dbow; 1 = dmpv
-worker_count = multiprocessing.cpu_count() - 1
+worker_count = cpu_count() - 1
 tokenize = '--tokenize' in sys.argv
-create_embeddings = '--embeddings' in sys.argv
 
 #input corpus
 train_corpus = sys.argv[1]
@@ -41,28 +37,26 @@ saved_path = sys.argv[2]
 #labels
 labels_file = sys.argv[3]
 
-with open(labels_file) as f:
+with open(labels_file, 'r') as f:
 	labels = f.read().splitlines()
 
 #enable logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-docs = []
+taggeddocs = []
+
+with open(train_corpus, 'r') as f:
+	docs = f.read().splitlines()
 
 if tokenize:
-	with open(train_corpus, 'r') as f:
-		docs = f.read().splitlines()
 	docs = nlp_clean(docs)
 
 for label, doc in zip(labels, docs):
-    td = TaggedDocument(words=line.split(), tags=[label])
-    docs.append(td)
+	td = TaggedDocument(words=tokenizer.tokenizer.split(doc.lower(), delimiter=' '), tags=[label])
+	#print(td)
+	taggeddocs.append(td)
 
-# model = g.Doc2Vec(window=8, dm = 0, hs=0, min_count=2, alpha=0.1, size=200, min_alpha=0.0001, epochs=50, workers=6, sample=1e-5, dbow_words=1, negative=5)
-
-model = g.Doc2Vec(size=vector_size, window=window_size, min_count=min_count, sample=sampling_threshold, workers=worker_count, hs=0, dm=dm, negative=negative_size, dbow_words=1, dm_concat=1, pretrained_emb=None, iter=train_epoch)
-model.build_vocab(docs)
-model.train(docs, total_examples=model.corpus_count, epochs=model.iter)
+model = g.Doc2Vec(taggeddocs, size=vector_size, window=window_size, min_count=min_count, sample=sampling_threshold, workers=worker_count, hs=0, dm=dm, negative=negative_size, dbow_words=1, dm_concat=1, pretrained_emb=None, iter=train_epoch)
 
 #save model
 model.save(saved_path)
