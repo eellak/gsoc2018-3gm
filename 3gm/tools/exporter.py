8978b301aec4db3a4afb375408f7cd5e29383cb1
Marios Papachristou <papachristoumarios@gmail.com>
2018-07-18 02:00:13 +0300

    Moved tools to tools package
    Former-commit-id: 46537125c735896fed6e56cae6ea4c4ff6546e95
    Former-commit-id: c1415cbeefae166a71348fcbeb56cb45ecf2c4d3

---
+++ 3gm/tools/exporter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Simple Exporter
+# usage: exporter.py --markdown < issue.txt > output.md
+import sys
+sys.path.insert(0, '../')
+import parser
+
+export_type = sys.argv[1].strip('--')
+issue = parser.IssueParser(None, stdin=True)
+laws = issue.detect_new_laws()
+for l in issue.new_laws:
+    try:
+        result = l.export_law(export_type)
+        sys.stdout.write(result)
+    except:
+        sys.stderr.write('Error in exporting')
+sys.stdout.flush()
--- 3gm/tools/law_codifier.py
+++ 3gm/tools/law_codifier.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import sys
 sys.path.insert(0, '../') 
 import codifier
