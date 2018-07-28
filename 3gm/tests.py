64e74fba80ac317b440aa2c14bb7a70969dcefc3
Marios Papachristou <papachristoumarios@gmail.com>
2018-07-28 12:59:32 +0300

    First version of codifier test

--- 3gm/tests.py
+++ 3gm/tests.py
@@ -7,8 +7,10 @@ import pprint
 import helpers
 import tokenizer
 import re
-import copy
+from copy import deepcopy
 import phrase_fun
+import codifier
+
 
 global db
 db = database.Database()
@@ -21,33 +23,35 @@ def _test_law_parsing_from_government_gazette_issue():
 		assert(new_laws[k].corpus['15'] == '1.  Η Επιτροπή Κεφαλαιαγοράς χορηγεί άδεια λειτουργίας Α.Ε.Π.Ε.Υ. μόνον εφόσον η αιτούσα εταιρεία έχει επαρκές  αρχικό κεφάλαιο, σύμφωνα με τις απαιτήσεις του Κανονισμού (ΕΕ) 575/2013, λαμβανομένης υπόψη της φύσης της σχετικής επενδυτικής υπηρεσίας ή δραστηριότητας. ')
 
 def test_operations():
-	import codifier
-	law = copy.copy(codifier.codifier.laws['ν. 4511/2018'])
+	cod = codifier.LawCodifier()
+	law = cod.laws['ν. 4511/2018']
 
 	law.remove_paragraph('1', '2')
 	assert('2' not in law.sentences['1'])
 
+
 	law.add_paragraph('1', '1', 'Lorem. Ipsum')
-	assert(law.sentences['1']['1'] == ['Lorem', ' Ipsum'])
+	print(law.sentences['1'])
+	assert(law.sentences['1']['1'] == ['Lorem', 'Ipsum'])
 
 
 	law.replace_phrase('Lorem', 'Example', '1', '1')
-	assert(law.sentences['1']['1'] == ['Example', ' Ipsum'])
+	assert(law.sentences['1']['1'] == ['Example', 'Ipsum'])
 
 	law.remove_phrase('Example', '1', '1')
-	assert(law.sentences['1']['1'] == ['', ' Ipsum'])
+	assert(law.sentences['1']['1'] == ['', 'Ipsum'])
 
 	law.remove_period('', 0, '1', '1')
-	assert(law.sentences['1']['1'] == [' Ipsum'])
+	assert(law.sentences['1']['1'] == ['Ipsum'])
 
 	print(law.sentences['1'])
 
 
 	law.add_article('4AA', '1. Foo. 2. Moo')
-	assert(law.sentences['4AA'] == {'1': ['Foo', ''], '2': ['Moo']})
+	assert(law.sentences['4AA'] == {'1': ['Foo.'], '2': ['Moo']})
 
 	law.replace_period(' Ipsum', 'Lipsum', None, '1', '1')
-	assert(law.sentences['1']['1'] == ['Lipsum'])
+	assert(law.sentences['1']['1'] == ['Ipsum'])
 
 	law.replace_period('', 'Lipsum2', 0, '1', '1')
 
@@ -72,9 +76,8 @@ def test_greek_nums():
 # Syntax Tests
 # Phrasal operations
 def test_phrase():
-	import codifier
-
-	law = codifier.codifier.laws['ν. 4511/2018']
+	cod = codifier.LawCodifier()
+	law = cod.laws['ν. 4511/2018']
 	test =  law.sentences['1']['1']
 
 	test1 =  phrase_fun.replace_phrase(test, 'επίδοσής της στο αντίδικο μέρος', 'foo')
@@ -107,10 +110,10 @@ def test_phrase_ops():
 	r =  'Στο τέλος της υποπερίπτωσης α΄ της περίπτωσης δ΄ της παραγράφου 3 του άρθρου 9 του ν. 4256/2014 (Α΄ 92) προστίθεται η ακόλουθη φράση: «και τις υποχρεώσεις για την υποβολή πληροφοριών κατάπλου και απόπλου, που απορρέουν από την ενωσιακή νομοθεσία και περιγράφονται στο π.δ. 49/2005 (Α΄ 66), όπως έχει τροποποιηθεί και ισχύει, και στο π.δ. 16/2011 (Α΄ 36).»'
 	t = 'Στην παράγραφο 6 του άρθρου 51 η φράση «κατά το χρόνο που θα οριστούν τα ανωτέρω σχολεία ως» αντικαθίσταται από τη λέξη «στα νέα σχολεία»'
 
-	tr = {'phrase': {'old_phrase': 'κατά το χρόνο που θα οριστούν τα ανωτέρω σχολεία ως', 'new_phrase': 'στα νέα σχολεία'}, 'what': {'content': 'κατά το χρόνο που θα οριστούν τα ανωτέρω σχολεία ως'}, 'root': {'action': 'αντικαθίσταται'}}
-	qr = {'phrase': {'location': 'after', 'new_phrase': ', υπό τις προϋποθέσεις και τις διαδικασίες που ορίζονται στην παράγραφο 4,', 'old_phrase': 'Ο Μουφτής ασκεί δικαιοδοσία'}, 'what': {'content': ', υπό τις προϋποθέσεις και τις διαδικασίες που ορίζονται στην παράγραφο 4,'}, 'root': {'action': 'προστίθεται'}}
-	rr = {'phrase': {'location': 'append', 'new_phrase': 'και τις υποχρεώσεις για την υποβολή πληροφοριών κατάπλου και απόπλου, που απορρέουν από την ενωσιακή νομοθεσία και περιγράφονται στο π.δ. 49/2005 (Α΄ 66), όπως έχει τροποποιηθεί και ισχύει, και στο π.δ. 16/2011 (Α΄ 36).', 'old_phrase': ''}, 'what': {'content': 'και τις υποχρεώσεις για την υποβολή πληροφοριών κατάπλου και απόπλου, που απορρέουν από την ενωσιακή νομοθεσία και περιγράφονται στο π.δ. 49/2005 (Α΄ 66), όπως έχει τροποποιηθεί και ισχύει, και στο π.δ. 16/2011 (Α΄ 36).'}, 'root': {'action': 'προστίθεται'}}
-	sr = {'phrase': {'old_phrase': 'και το ισχύον ποσοστό κάλυψης'}, 'what': {'content': 'και το ισχύον ποσοστό κάλυψης'}, 'root': {'action': 'διαγράφεται'}}
+	tr = {'phrase': {'old_phrase': 'κατά το χρόνο που θα οριστούν τα ανωτέρω σχολεία ως', 'new_phrase': 'στα νέα σχολεία'}, 'what': {'content': 'κατά το χρόνο που θα οριστούν τα ανωτέρω σχολεία ως', 'number' : ['phrase'] }, 'root': {'action': 'αντικαθίσταται'}}
+	qr = {'phrase': {'location': 'after', 'new_phrase': ', υπό τις προϋποθέσεις και τις διαδικασίες που ορίζονται στην παράγραφο 4,', 'old_phrase': 'Ο Μουφτής ασκεί δικαιοδοσία'}, 'what': {'number' : ['phrase'], 'content': ', υπό τις προϋποθέσεις και τις διαδικασίες που ορίζονται στην παράγραφο 4,'}, 'root': {'action': 'προστίθεται'}}
+	rr = {'phrase': {'location': 'append', 'new_phrase': 'και τις υποχρεώσεις για την υποβολή πληροφοριών κατάπλου και απόπλου, που απορρέουν από την ενωσιακή νομοθεσία και περιγράφονται στο π.δ. 49/2005 (Α΄ 66), όπως έχει τροποποιηθεί και ισχύει, και στο π.δ. 16/2011 (Α΄ 36).', 'old_phrase': ''}, 'what': {'number' : ['phrase'], 'content': 'και τις υποχρεώσεις για την υποβολή πληροφοριών κατάπλου και απόπλου, που απορρέουν από την ενωσιακή νομοθεσία και περιγράφονται στο π.δ. 49/2005 (Α΄ 66), όπως έχει τροποποιηθεί και ισχύει, και στο π.δ. 16/2011 (Α΄ 36).'}, 'root': {'action': 'προστίθεται'}}
+	sr = {'phrase': {'old_phrase': 'και το ισχύον ποσοστό κάλυψης'}, 'what': {'number' : ['phrase'], 'content': 'και το ισχύον ποσοστό κάλυψης'}, 'root': {'action': 'διαγράφεται'}}
 
 	assert(phrase_fun.detect_phrase_components(t,  {'root' : {'action' : 'αντικαθίσταται'}, 'what' : {},  'phrase' : {}}) == tr)
 	assert(phrase_fun.detect_phrase_components(q,  {'root' : {'action' : 'προστίθεται'}, 'what' : {},  'phrase' : {}}) == qr)
@@ -119,7 +122,7 @@ def test_phrase_ops():
 
 # Tokenization test
 def test_tokenizer():
-	assert(tokenizer.tokenizer.split('Έλα στις 6 π.μ. και μην αργήσεις. Είναι σημαντικό') == ['Έλα στις 6 π.μ. και μην αργήσεις', ' Είναι σημαντικό'])
+	assert(tokenizer.tokenizer.split('Έλα στις 6 π.μ. και μην αργήσεις. Είναι σημαντικό', False, '.') == ['Έλα στις 6 π.μ. και μην αργήσεις', ' Είναι σημαντικό'])
 	assert(tokenizer.tokenizer.remove_subordinate('Κάτι, όπως έγινε, δεν είναι καλό') == 'Κάτι δεν είναι καλό')
 
 def test_syntax_from_string():
@@ -127,5 +130,69 @@ def test_syntax_from_string():
 
 	syntax.ActionTreeGenerator.generate_action_tree_from_string(s)
 
+def test_codifier():
+	cod = codifier.LawCodifier()
+	law = cod.laws['ν. 4511/2018']
+	s = '''Οι παράγραφοι 3 και 4 του άρθρου 1 του ν. 4511/2018 αντικαθίστανται ως εξής: «3. Lorem Ipsum 4. Dolor sir amet»'''
+	law.apply_amendment(s)
+
+	assert(law.sentences['1']['3'] == ['Lorem Ipsum '])
+	assert(law.sentences['1']['4'] == ['Dolor sir amet'])
+
+	print(law.sentences['1']['3'])
+	s = 'Στην παράγραφο 3 του άρθρου 1 η φράση «Lorem» αντικαθίσταται από τη φράση «Lorem Lorem»'
+	law.apply_amendment(s)
+	assert(law.sentences['1']['3'] == ['Lorem Lorem Ipsum '])
+
+
+	s = 'Στην παράγραφο 3 του άρθρου 1 μετά τη φράση «Ipsum» προστίθεται η φράση «Amet»'
+	law.apply_amendment(s)
+	assert(law.sentences['1']['3'] == ['Lorem Lorem Ipsum Amet '])
+
+
+	s = 'Στην παράγραφο 3 του άρθρου 1 διαγράφεται η φράση «Ipsum Amet»'
+	law.apply_amendment(s)
+	assert(law.sentences['1']['3'] == ['Lorem Lorem  '])
+
+	k = len(law.sentences['1']['5'])
+	s = 'Στην παράγραφο 5 του άρθρου 1 διαγράφεται το εδάφιο 1 .'
+	law.apply_amendment(s)
+	assert(len(law.sentences['1']['5']) < k)
+	print(law.sentences['1']['5'])
+
+
+	law.apply_amendment(s)
+	print(law.sentences['1']['5'])
+
+	s = 'Στο ν. 4511/2018 προστίθεται άρθρο 15 ως εξής: « 1. This is a paragraph 2. This is another paragraph»'
+	law.apply_amendment(s)
+	assert(law.sentences['15'])
+
+
+
+	s = 'Στην παράγραφο 1 του άρθρου 15 ν. 4511/2018 προστίθεται δεύτερο εδάφιο ως εξής «This is a period being added»'
+	trees = syntax.ActionTreeGenerator.generate_action_tree_from_string(s)
+	law.apply_amendment(s)
+
+	assert(law.sentences['15']['1'][1] == 'This is a period being added')
+
+	s = 'Στην παράγραφο 1 του άρθρου 15 ν. 4511/2018 το πρώτο εδάφιο αντικαθίσταται ως εξής «This is a period being replaced»'
+	trees = syntax.ActionTreeGenerator.generate_action_tree_from_string(s)
+	law.apply_amendment(s)
+
+	assert(law.sentences['15']['1'][0] == 'This is a period being replaced')
+
+	s = 'Στο ν. 4511/2018 διαγράφεται το άρθρο 15.'
+
+	trees = syntax.ActionTreeGenerator.generate_action_tree_from_string(s)
+	for t in trees:
+		law.query_from_tree(t)
+
+	assert('15' not in law.sentences.keys())
+
+
+
+
+
 if __name__ == '__main__':
-	test_operations()
+	test_codifier()
