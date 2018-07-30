c028921c5c50e9397c07718303a3b2cf9852bae2
Marios Papachristou <papachristoumarios@gmail.com>
2018-07-30 19:08:50 +0300

    Build pipeline supports env vars

--- build_pipeline.py
+++ build_pipeline.py
@@ -9,9 +9,16 @@ pipeline_depth = {
     'versions' : 3
 }
 
+# data dir
+try:
+    data_dir = os.environ['CODIFIER_DATA']
+except KeyError:
+    print('Please export CODIFIER_DATA')
+    sys.exit(0)
+
 pipeline = sorted(sys.argv[1:], key = lambda x: pipeline_depth[x])
 sys.path.insert(0, './3gm')
 import codifier
 print('Building codifier')
-codifier.build(start=1999, end=2018, data_dir='./data', pipeline=pipeline)
+codifier.build(start=1999, end=2018, data_dir=data_dir, pipeline=pipeline)
 print('Complete')
