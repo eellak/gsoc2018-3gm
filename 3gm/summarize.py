0f5cb45ccf4254e78dbabb61e8ec5850bda8cc0c
Marios Papachristou <papachristoumarios@gmail.com>
2018-08-02 21:03:43 +0300

    Resolve name conflict

--- 3gm/summarize.py
+++ 3gm/summarize.py
@@ -1,11 +1,12 @@
 # Produce summaries of laws from law titles using
 # TextRank algorithm provided by gensim
 
-from gensim.summarization import summarize
+from gensim.summarization import summarize as summarize_textrank
 import codifier
 import multiprocessing
 import database
 import string
+import logging
 db = database.Database()
 
 # Filtering Heuristic
@@ -20,21 +21,22 @@ def job(identifier):
         titles = filter(lambda x: len(x.split()) <= MAX_TITLE_WORDS, [x.lstrip().rstrip().translate(punct) for x in l.titles.values()])
         titles = filter(lambda x: x.rstrip() != '', titles)
         titles = '. '.join(titles)
-        summary = summarize(titles, ratio=0.1)
+        summary = summarize_textrank(titles, ratio=0.1)
         summary_obj = {
             '_id' : identifier,
             'summary' : summary
         }
         db.summaries.save(summary_obj)
-    except:
-        pass
+    except BaseException as e:
+        logging.warning(str(e))
 
 # Summarize
-def summarize(*identifiers):
+def summarize(identifiers=[]):
     workers = multiprocessing.cpu_count() - 1
     pool = multiprocessing.Pool(workers)
-    if list(identifiers) == []:
+    if identifiers == []:
         identifiers = list(codifier.codifier.laws.keys())
+    print(identifiers)
     pool.map(job, identifiers)
 
 if __name__ == '__main__':
