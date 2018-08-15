0a7a4bca3183a569143784b4a9869eade362a2be
Marios Papachristou <papachristoumarios@gmail.com>
2018-08-15 12:41:48 +0300

    Add issue B to fetch_daily

--- scripts/fetch_daily.sh
+++ scripts/fetch_daily.sh
@@ -5,4 +5,5 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 TODAY=`date +%d.%m.%Y` &&
 YESTERDAY=`date +%d.%m.%Y -d "-2 days"` &&
-./fetcher.py -date_from $YESTERDAY -date_to $TODAY -output_dir $1
+./fetcher.py -date_from $YESTERDAY -date_to $TODAY -output_dir $1 --type Α --upload
+./fetcher.py -date_from $YESTERDAY -date_to $TODAY -output_dir $1 --type Β --upload
