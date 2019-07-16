2e39d035078f9c026fc09779619520c02b172468
Sotiris <sotirispapadiamantis@gmail.com>
2019-07-16 15:49:37 +0100

    Update script

--- scripts/language_model.py
+++ scripts/language_model.py
@@ -8,6 +8,66 @@ from keras.layers import LSTM
 from keras.layers import Embedding
 import spacy
 
+
+def get_features(docs, max_length):
+    docs = list(docs)
+    Xs = numpy.zeros((len(docs), max_length), dtype="int32")
+    for i, doc in enumerate(docs):
+        j = 0
+        for token in doc:
+            vector_id = token.vocab.vectors.find(key=token.orth)
+            if vector_id >= 0:
+                Xs[i, j] = vector_id
+            else:
+                Xs[i, j] = 0
+            j += 1
+            if j >= max_length:
+                break
+    return Xs
+
+
+
+
+
+def train(
+    train_texts,
+    train_labels,
+    dev_texts,
+    dev_labels,
+    lstm_shape,
+    lstm_settings,
+    lstm_optimizer,
+    batch_size=100,
+    nb_epoch=5,
+    by_sentence=True,
+):
+    print("Loading spaCy")
+    nlp = spacy.load("blank_vectors")
+    nlp.add_pipe(nlp.create_pipe("sentencizer"))
+    embeddings = get_embeddings(nlp.vocab)
+    model = compile_lstm(embeddings, lstm_shape, lstm_settings)
+
+    print("Parsing texts...")
+    train_docs = list(nlp.pipe(train_texts))
+    dev_docs = list(nlp.pipe(dev_texts))
+    if by_sentence:
+        train_docs, train_labels = get_labelled_sentences(train_docs, train_labels)
+        dev_docs, dev_labels = get_labelled_sentences(dev_docs, dev_labels)
+
+    train_X = get_features(train_docs, lstm_shape["max_length"])
+    dev_X = get_features(dev_docs, lstm_shape["max_length"])
+    model.fit(
+        train_X,
+        train_labels,
+        validation_data=(dev_X, dev_labels),
+        epochs=nb_epoch,
+        batch_size=batch_size,
+    )
+    return model
+
+
+
+
 def get_embeddings(vocab):
     return vocab.vectors.data
 
@@ -44,7 +104,19 @@ def compile_model(embeddings, shape, settings):
 
 
 def generate_text(seed_text, next_words, max_sequence_len, model):
-      
+    for j in range(next_words):
+        token_list = tokenizer.texts_to_sequences([seed_text])[0]
+        token_list = pad_sequences([token_list], maxlen= 
+                             max_sequence_len-1, padding='pre')
+        predicted = model.predict_classes(token_list, verbose=0)
+  
+        output_word = ""
+        for word, index in tokenizer.word_index.items():
+            if index == predicted:
+                output_word = word
+                break
+        seed_text += " " + output_word
+ return seed_text      
 
 
 if __name__ == "__main__":
