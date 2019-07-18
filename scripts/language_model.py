53d6e92c2b7c88724078aef6d00b73390803a3cf
Sotiris <sotirispapadiamantis@gmail.com>
2019-07-18 21:58:48 +0100

    Fix embeddings to model integration

--- scripts/language_model.py
+++ scripts/language_model.py
@@ -1,190 +1,85 @@
-
-from numpy import array
+from keras.preprocessing.sequence import pad_sequences
+from keras.layers import Embedding, LSTM, Dense, Dropout
 from keras.preprocessing.text import Tokenizer
-from keras.utils import to_categorical
+from keras.callbacks import EarlyStopping
 from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
-from keras.layers import Embedding
+from keras.utils import np_utils
+import keras.utils as ku 
+import numpy as np 
 import spacy
 
-
-def get_features(docs, max_length):
-    docs = list(docs)
-    Xs = numpy.zeros((len(docs), max_length), dtype="int32")
-    for i, doc in enumerate(docs):
-        j = 0
-        for token in doc:
-            vector_id = token.vocab.vectors.find(key=token.orth)
-            if vector_id >= 0:
-                Xs[i, j] = vector_id
-            else:
-                Xs[i, j] = 0
-            j += 1
-            if j >= max_length:
-                break
-    return Xs
-
-
-
-def train(
-    train_texts,
-    train_labels,
-    dev_texts,
-    dev_labels,
-    lstm_shape,
-    lstm_settings,
-    lstm_optimizer,
-    batch_size=100,
-    nb_epoch=5,
-    by_sentence=True,
-):
-    print("Loading spaCy")
-    nlp = spacy.load("blank_vectors")
-    nlp.add_pipe(nlp.create_pipe("sentencizer"))
-    embeddings = get_embeddings(nlp.vocab)
-    model = compile_lstm(embeddings, lstm_shape, lstm_settings)
-
-    print("Parsing texts...")
-    train_docs = list(nlp.pipe(train_texts))
-    dev_docs = list(nlp.pipe(dev_texts))
-    if by_sentence:
-        train_docs, train_labels = get_labelled_sentences(train_docs, train_labels)
-        dev_docs, dev_labels = get_labelled_sentences(dev_docs, dev_labels)
-
-    train_X = get_features(train_docs, lstm_shape["max_length"])
-    dev_X = get_features(dev_docs, lstm_shape["max_length"])
-    model.fit(
-        train_X,
-        train_labels,
-        validation_data=(dev_X, dev_labels),
-        epochs=nb_epoch,
-        batch_size=batch_size,
-    )
-    return model
-
-
-
+tokenizer = Tokenizer()
 
 def get_embeddings(vocab):
-    return vocab.vectors.data
+	return vocab.vectors.data
 
 
-def compile_model(embeddings, shape, settings):
-    model = Sequential()
-    model.add(
-        Embedding(
-            embeddings.shape[0],
-            embeddings.shape[1],
-            input_length=shape["max_length"],
-            trainable=False,
-            weights=[embeddings],
-            mask_zero=True,
-        )
-    )
-    model.add(TimeDistributed(Dense(shape["nr_hidden"], use_bias=False)))
-    model.add(
-        Bidirectional(
-            LSTM(
-                shape["nr_hidden"],
-                recurrent_dropout=settings["dropout"],
-                dropout=settings["dropout"],
-            )
-        )
-    )
-    model.add(Dense(shape["nr_class"], activation="sigmoid"))
-    model.compile(
-        optimizer=Adam(lr=settings["lr"]),
-        loss="binary_crossentropy",
-        metrics=["accuracy"],
-    )
-    return model
+def create_model(embeddings):
+	model = Sequential()
+	model.add(Embedding(embeddings.shape[0],embeddings.shape[1],input_length=150,trainable=True,weights=[embeddings],mask_zero=True))
+	model.add(LSTM(150, return_sequences = True))
+	model.add(Dropout(0.2))
+	model.add(LSTM(100))
+	model.add(Dense(84, activation='softmax'))
+
+	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 
-def generate_text(seed_text, next_words, max_sequence_len, model):
-    for j in range(next_words):
-        token_list = tokenizer.texts_to_sequences([seed_text])[0]
-        token_list = pad_sequences([token_list], maxlen= 
-                             max_sequence_len-1, padding='pre')
-        predicted = model.predict_classes(token_list, verbose=0)
-  
-        output_word = ""
-        for word, index in tokenizer.word_index.items():
-            if index == predicted:
-                output_word = word
-                break
-        seed_text += " " + output_word
- return seed_text 
+	print(model.summary())
+	return model 
 
-@plac.annotations(
-    train_dir=("Location of training file or directory"),
-    dev_dir=("Location of development file or directory"),
-    model_dir=("Location of output model directory",),
-    is_runtime=("Demonstrate run-time usage", "flag", "r", bool),
-    nr_hidden=("Number of hidden units", "option", "H", int),
-    max_length=("Maximum sentence length", "option", "L", int),
-    dropout=("Dropout", "option", "d", float),
-    learn_rate=("Learn rate", "option", "e", float),
-    nb_epoch=("Number of training epochs", "option", "i", int),
-    batch_size=("Size of minibatches for training LSTM", "option", "b", int),
-    nr_examples=("Limit to N examples", "option", "n", int),
-)     
-
-def main(
-    model_dir=None,
-    train_dir=None,
-    dev_dir=None,
-    is_runtime=False,
-    nr_hidden=64,
-    max_length=100,  # Shape
-    dropout=0.5,
-    learn_rate=0.001,  # General NN config
-    nb_epoch=5,
-    batch_size=256,
-    nr_examples=-1,
-):  # Training params
-    if model_dir is not None:
-        model_dir = pathlib.Path(model_dir)
-    if train_dir is None or dev_dir is None:
-        imdb_data = thinc.extra.datasets.imdb()
-    if is_runtime:
-        if dev_dir is None:
-            dev_texts, dev_labels = zip(*imdb_data[1])
-        else:
-            dev_texts, dev_labels = read_data(dev_dir)
-        acc = evaluate(model_dir, dev_texts, dev_labels, max_length=max_length)
-        print(acc)
-    else:
-        if train_dir is None:
-            train_texts, train_labels = zip(*imdb_data[0])
-        else:
-            print("Read data")
-            train_texts, train_labels = read_data(train_dir, limit=nr_examples)
-        if dev_dir is None:
-            dev_texts, dev_labels = zip(*imdb_data[1])
-        else:
-            dev_texts, dev_labels = read_data(dev_dir, imdb_data, limit=nr_examples)
-        train_labels = numpy.asarray(train_labels, dtype="int32")
-        dev_labels = numpy.asarray(dev_labels, dtype="int32")
-        lstm = train(
-            train_texts,
-            train_labels,
-            dev_texts,
-            dev_labels,
-            {"nr_hidden": nr_hidden, "max_length": max_length, "nr_class": 1},
-            {"dropout": dropout, "lr": learn_rate},
-            {},
-            nb_epoch=nb_epoch,
-            batch_size=batch_size,
-        )
-        weights = lstm.get_weights()
-        if model_dir is not None:
-            with (model_dir / "model").open("wb") as file_:
-                pickle.dump(weights[1:], file_)
-            with (model_dir / "config.json").open("w") as file_:
-                file_.write(lstm.to_json())
+def generate_text(seed_text, next_words, max_sequence_len):
+	for _ in range(next_words):
+		token_list = tokenizer.texts_to_sequences([seed_text])[0]
+		token_list = pad_sequences([token_list], maxlen=max_sequence_len-1, padding='pre')
+		predicted = model.predict_classes(token_list, verbose=1)
+		
+		output_word = ""
+		for word, index in tokenizer.word_index.items():
+			if index == predicted:
+				output_word = word
+				break
+		seed_text += " " + output_word
+	return seed_text
 
 
 
-if __name__ == "__main__":
-    plac.call(main)
+
+
+filename = "20180100009.txt"
+raw_text = open(filename).read()
+raw_text = raw_text.lower()
+# create mapping of unique chars to integers
+chars = sorted(list(set(raw_text)))
+char_to_int = dict((c, i) for i, c in enumerate(chars))
+# summarize the loaded data
+n_chars = len(raw_text)
+n_vocab = len(chars)
+print("Total Characters: ", n_chars)
+print("Total Vocab: ", n_vocab)
+# prepare the dataset of input to output pairs encoded as integers
+seq_length = 150
+dataX = []
+dataY = []
+for i in range(0, n_chars - seq_length, 1):
+	seq_in = raw_text[i:i + seq_length]
+	seq_out = raw_text[i + seq_length]
+	dataX.append([char_to_int[char] for char in seq_in])
+	dataY.append(char_to_int[seq_out])
+n_patterns = len(dataX)
+X = np.reshape(dataX, (n_patterns, seq_length))
+# normalize
+X = X / float(n_vocab)
+# one hot encode the output variable
+y = np_utils.to_categorical(dataY)
+
+nlp = spacy.load("blank_vectors")
+#nlp.add_pipe(nlp.create_pipe("sentencizer"))
+embeddings = get_embeddings(nlp.vocab)
+print(len(embeddings))
+model = create_model(embeddings)
+earlystop = EarlyStopping(monitor='val_loss', min_delta=0, patience=5, verbose=0, mode='auto')
+seed_file="20180100009.txt"
+seed_text = open(seed_file).read()
+model.fit(X, y, epochs=1, verbose=1)
+print(generate_text(" Η Ενεργειακή Κοινότητα (Ε.Κοιν.) είναι αστικός συνεταιρισμός αποκλειστικού σκοπού με στόχο την προώθηση της κοινωνικής και αλληλέγγυας οικονομίας, όπως ορίζεται στην παρ. 1 του άρθρου 2 του ν. 4430/2016 ", 3, 151))
