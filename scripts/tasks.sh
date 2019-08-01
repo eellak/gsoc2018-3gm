bd83082c8d56b1798295b0fdba0118400c23695b
Marios Papachristou <papachristoumarios@gmail.com>
2019-08-01 23:13:28 +0300

    Fix identation

--- scripts/tasks.sh
+++ scripts/tasks.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-# this is a sample bash script that demonstrates how we can use
+# This is a sample bash script that demonstrates how we can use
 # the new fetching script fetch by issue to mine several issues of the 
 # GGG. 
 
-for (( i=1; i<=800; i=i+200 ))
-do
-end=$(($i+199))
-python3 fetch_by_issue.py -issue_from $i -issue_to $end -year 1984 -output_dir ./issues \
---chromedriver chromedriver --type  Δ
-sleep 5
+for (( i=1; i<=800; i=i+200 )); do
+   end=$(($i+199))
+   python3 fetch_by_issue.py -issue_from $i -issue_to $end -year 1984 -output_dir ./issues \
+   --chromedriver chromedriver --type  Δ
+  sleep 5
 done
 
 
